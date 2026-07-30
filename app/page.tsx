d00d1951f6298103de1c87fc91ac440939904fcb
ezm-dev <ezm.sabry@gmail.com>
2026-07-30 10:52:47 +1000

    fix: correct import statement and variable name for styles in page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styless from '@/app/ui/home.module.css';
+import styles from '@/app/ui/home.module.css';
 
 
 export default function Page() {
@@ -13,7 +13,7 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           {/* Apply style to component through CSS modules */}
-          <div className={styless.shape} />
+          <div className={styles.shape} />
           {/* Apply styles using Tailwind CSS */}
           <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
