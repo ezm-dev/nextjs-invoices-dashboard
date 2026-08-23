bdd66e19f21481be90720add8271e7761983867d
ezm-dev <ezm.sabry@gmail.com>
2026-08-24 09:46:19 +1000

    fix: update allowed DevOrigins for Development

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev']
- history: {
-  type: 'memory',
-  limit: 100,
-  ttl: 3600,
- },
- experimental: {
-  appDir: true,
 
 }
-
 export default nextConfig;
