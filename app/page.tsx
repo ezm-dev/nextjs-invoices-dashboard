c0a746cd7d88959a9aa11452333d4b180431553a
ezm-dev <ezm.sabry@gmail.com>
2026-08-24 10:26:27 +1000

    feat: add logo component to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           
           {/* Apply style to component through CSS modules */}
-          {/* <div className={styles.shape} /> */}
+          <div className={styles.shape} />
           
           {/* Apply styles using Tailwind CSS */}
           <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
