b12fe9b4f1a91839ad0a918f805c42de2c476870
ezm-dev <ezm.sabry@gmail.com>
2026-09-14 14:39:51 +1000

    feat: Enhance Invoices page with search and table components, and implement Suspense for loading

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
- allowedDevOrigins: [   
-     "localhost",
-    "127.0.0.1"]
+
+allowedDevOrigins: ['192.168.20.155']
 
 }
 
+
+
+export default nextConfig;
